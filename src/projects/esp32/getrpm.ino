28954408e2d5b41687fb1adc7ce0fe212961a13e
josenaldeOliveira <josenalde@gmail.com>
2022-01-10 21:28:31 -0300

    added esp32 adapts

--- src/projects/esp32/getrpm.ino
+++ src/projects/esp32/getrpm.ino
@@ -1,10 +1,24 @@
 volatile int rpm;
+/* volatile plays an important role in C programming as the compiler can’t guess about the value. 
+The main reason behind using volatile is that it can change value any time a user wants it to be changed or
+when another thread is running but using the same variable.
+It is therefore useful when interrupt service routines can change variable values, instead of the code itself
+
+See examples and explanation here: https://www.educba.com/volatile-in-c/
+*/
+
 unsigned long elapsedTime = 0;
-int intervalTimer = 1000;
+int intervalTimer = 1000; //1s
 void setup()
 {
    Serial.begin(115200);//Inicia a Serial.
-   attachInterrupt(digitalPinToInterrupt(2), ctt, RISING);//Habilita a interrupção 0 no pino 2. UNO
+   //https://www.filipeflop.com/blog/uso-de-interrupcoes-externas-com-esp32/
+   /* attachInterrupt(GPIOPin, ISR, Mode)
+      ISR - função a ser executada
+      Mode - LOW, HIGH, RISING, FALLING ou CHANGE
+   */
+   pinMode(2, INPUT); //setar GPIO de interrupção como INPUT
+   attachInterrupt(2, ctt, RISING);//Habilita a interrupção 0 no pino 2. UNO. o Uno só possui 2 pinos para interrupção. Esp32 qualquer digital
    elapsedTime = millis();
 }
 void loop()
@@ -24,3 +38,10 @@ void ctt()
 {
    rpm++;//Incrementa a váriavel.
 }
+
+// no caso do ESP32, necessário keyword IRAM_ATTR para indicar que a função é executada em RAM interna do ESP32 e não na FLASH, para ser mais rápido
+/* 
+void IRAM_ATTR ctt() {
+   rpm++;//Incrementa a váriavel.
+} 
+*/
